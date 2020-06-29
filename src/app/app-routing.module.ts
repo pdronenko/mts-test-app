d0a1c2db2c5b693ea52a3b216d75eaf032346e9b
Pavel Dronenko <p.dronenko@wellyes.ru>
2020-06-29 22:49:12 +0300

    add app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ChannelsComponent } from './channels/channels.component';
+import { EmptyPageComponent } from './empty-page/empty-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'channels', component: ChannelsComponent },
+  { path: 'first', component: EmptyPageComponent },
+  { path: 'second', component: EmptyPageComponent },
+  { path: '**', redirectTo: 'channels' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
