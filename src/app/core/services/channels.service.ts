fbf49e06ead89cd25ced5177ebafd6a8f14069ad
Pavel Dronenko <p.dronenko@wellyes.ru>
2020-06-29 22:53:59 +0300

    add channel service

---
+++ src/app/core/services/channels.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { IChannelsResponse } from '../interfaces/channels-response.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChannelsService {
+  constructor(private http: HttpClient) { }
+
+  getChannels(): Observable<IChannelsResponse> {
+    return this.http.get<IChannelsResponse>('../../assets/channels.json');
+  }
+}
