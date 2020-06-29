844c119520db25cb0c0bf4fcaebe96a7869414de
Pavel Dronenko <p.dronenko@wellyes.ru>
2020-06-29 22:54:51 +0300

    add channel info component

---
+++ src/app/channel-info/channel-info.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { IChannel } from '../core/interfaces/channel.interface';
+
+@Component({
+  selector: 'app-channel-info',
+  templateUrl: './channel-info.component.html',
+  styleUrls: ['./channel-info.component.scss']
+})
+export class ChannelInfoComponent implements OnInit {
+  @Input() channel: IChannel;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
